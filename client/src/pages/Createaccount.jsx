c0930323df1512ce65d0f6ab7c07c91af7c25eb7
skrijwan <skrijwan329@gmail.com>
2025-04-20 00:38:07 +0530

    Add registration functionality and integrate new components
    - Implement Register component for user registration with file upload
    - Update Createaccount page to include Register component and display registration GIF
    - Add new styles for input file and adjust layout in App.css
    - Include Axios for API calls in Register component
    - Clean up console logs in EmailVerify component
    - Add new GIF assets for registration process

--- client/src/pages/Createaccount.jsx
+++ client/src/pages/Createaccount.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { useEmail } from '../contexts/emailContext'
+import Register from '../component/Register'
+import gifnew from "../assets/gifs/registriongif.gif"
 export default function Createaccount() {
-  const emailContext = useEmail()
-  const [emailV] = emailContext || []
   return (
-    <div>
-      <input className='text-black' type="email" value={emailV} readOnly />
-      <img src={gif} alt="" />
+    <div className='w-full  flex items-center justify-evenly'>
+    <Register />
+    <div className=' w-[30%]'>
+      <img className='h-[460px]' src={gifnew} alt="" />
     </div>
+  </div>
   )
 }

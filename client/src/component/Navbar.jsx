46de98534cb8113707489afee989e791ead84479
Akash Tiwari <84653553+Ak4ssh@users.noreply.github.com>
2025-05-01 14:27:59 +0530

    Update Navbar.jsx

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -14,8 +14,8 @@ const Navbar = () => {
             <nav className="bg-white dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">undefined</span>
+                    <img src="https://envs.sh/ltG.jpg" className="h-8 w-8 rounded-full" alt="Flowbite Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Smart Canteen</span>
                 </Link>
                 <ProfileNav/>
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">

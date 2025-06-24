60db691e756ff948000618ee699ad85ffa6a6850
skrijwan <skrijwan329@gmail.com>
2025-06-25 01:12:50 +0530

    feat: Add error handling in Addfood component

--- client/src/pages/Addfood.jsx
+++ client/src/pages/Addfood.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Addfoood from '../component/Addfoood'
+import { handleError } from '../component/ErrorMessage'
 export default function Addfood() {
+    useEffect(()=>{
+        handleError('not allow')
+    })
     return (
         <div>
             <div className='w-full h-[85vh] flex items-center justify-center'>
